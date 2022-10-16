d2623e5fc0934f2cbe48262428e995c7bafe94be
Yana Nashchetska <yana.nashchetska.ri.2022@lpnu.ua>
2022-10-16 15:24:03 +0300

    new commit

--- Lab_03_3/Lab_03_3.cpp
+++ Lab_03_3/Lab_03_3.cpp
@@ -11,30 +11,28 @@ using namespace std;
 int main()
 {
 	double x; // вхідний аргумент
-	double R; // вхідний параметр
+	double R1; // вхідний параметр
+	double R2; // вхідний результат
 	double y; // результат обчислення виразу
 
-	cout << "R = "; cin >> R;
+	cout << "R1 = "; cin >> R1;
+	cout << "R2 = "; cin >> R2;
 	cout << "x = "; cin >> x;
 
 	// розгалуження в повній формі
-	if (x <= -R)
-		y = (R * (x + 2)) / (R - 2);
-
+	if (x <= -R1)
+		y = ((x + 2) * (-R1)) / (2 - R1);
 	else
-		if (-R < x && x <= 0)
-			y = sqrt(R * R - x * x) - R;
-
+		if (-R1 < x && x <= 0)
+			y = -R1 + sqrt(R1 * R1 - x * x);
 		else
-			if (0 < x && x <= 2 * R)
-				y = 2 * R - sqrt(4 * R * R - x * x);
-
+			if (0 < x && x <= 2 * R2)
+				y = R2 - sqrt(R2 * R2 - x * x);
 			else
-				if (2 * R <= x <= 4)
-					y = -R;
-
+				if (R2 <= x <= 4)
+					y = -R1;
 				else
-					y = (R * (x - 6)) / 2;
+					y = (R1 * (x - 6)) / 2.0;
 
 	cout << endl;
 	cout << "y = " << y << endl;
